e7f6027037e85dd371c7d93d403c4ec0b855797c
sabilimaulana <spacysbl.learn@gmail.com>
2022-07-31 21:01:09 +0700

    add time to table user and admin

--- pkg/models/admin.go
+++ pkg/models/admin.go
@@ -1,6 +1,15 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Admin struct {
-	UserUUID string `json:"uuid"`
-	User     User   `json:"user" gorm:"foreignKey:UserUUID"`
+	UserUUID  string         `json:"uuid"`
+	User      User           `json:"user" gorm:"foreignKey:UserUUID"`
+	CreatedAt time.Time      `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time      `json:"updated_at" gorm:"autoUpdateTime:nano"` // Use unix nano seconds as updating time
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
 }
--- pkg/models/auth.go
+++ pkg/models/auth.go
@@ -1,7 +1,16 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type User struct {
-	UUID     string `json:"uuid" gorm:"primaryKey"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	UUID      string         `json:"uuid" gorm:"primaryKey"`
+	Email     string         `json:"email"`
+	Password  string         `json:"password"`
+	CreatedAt time.Time      `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time      `json:"updated_at" gorm:"autoUpdateTime:nano"` // Use unix nano seconds as updating time
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
 }
